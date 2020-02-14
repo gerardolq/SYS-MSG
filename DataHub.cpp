b348e16d2dc01ebdd55c59773e5fe76ef214429d
gerardolq <58379792+gerardolq@users.noreply.github.com>
2020-02-13 17:37:10 -0800

    Add files via upload

--- DataHub.cpp
+++ DataHub.cpp
@@ -14,6 +14,8 @@ struct buf
 	char greeting[50];
 };
 
+
+
 int main()
 {
 	int beta = 257;
@@ -21,16 +23,48 @@ int main()
 	int qid = msgget(ftok(".",'u'),IPC_EXCL|IPC_CREAT|0600);
 	buf msg;
 	int size = sizeof(msg) - sizeof(long);
-	buf rcv;
-	msg.m_type = 4;
-	strncpy(msg.greeting, "Fake message",size);
-	msgsnd(qid, (struct msgbuf *)&msg, size, 0);
-	msgrcv(qid, (struct msgbuf *)&rcv, size, 997, 0);
-	cout << getpid() << endl;
-	cout << rcv.greeting << endl;
-	msgrcv(qid, (struct msgbuf *)&rcv, size, 997, 0);
-	cout << getpid() << endl;
-	cout << rcv.greeting << endl;
+	buf rcvA;
+	buf rcvB;
+	buf rcvC;
+	int msgRec = 0;
+	
+	while(rcvA.greeting[0]!='T' || msgRec < 10001){
+
+		if(msgRec > 10000)
+		{
+			msg.m_type = 3;
+			cout << "WE HIT 10000!!!!" << endl;
+			strncpy(msg.greeting, "T", size);
+			msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+			cout<<"booooo"<<endl;
+			msgRec++;
+		}
+
+		if(msgrcv(qid, (struct msgbuf *)&rcvA, size, 997, IPC_NOWAIT) != -1)
+		{
+			//cout <<"DataHub Receiving from ProbeA (process ID): " << getpid() << endl;
+			//cout << rcvA.greeting << endl << endl;
+			//msgRec++;
+			msg.m_type = 4;
+			strncpy(msg.greeting, rcvA.greeting, size);
+			if(rcvA.greeting[0] != 'T')
+				msgsnd(qid, (struct msgbuf *)&msg, size, 0); // sending to probeA
+		}
+
+		if(msgrcv(qid, (struct msgbuf *)&rcvB, size, 257, IPC_NOWAIT) != -1)
+		{
+			cout <<"DataHub Receiving from ProbeB (process ID): " << getpid() << endl;
+			cout << "message " << msgRec;
+			cout << rcvB.greeting << endl << endl;
+			msgRec++;
+		}
+
+	
+	}
+
+	//msgrcv(qid, (struct msgbuf *)&rcv, size, 997, 0);
+	//cout << getpid() << endl;
+	//cout << rcv.greeting << endl;
 	msgctl(qid,IPC_RMID,NULL);
 
 	return 0;
--- ProbeB.cpp
+++ ProbeB.cpp
@@ -33,7 +33,7 @@ string toString(int n)
 
 int main()
 {
-	int alpha = 997;
+	int beta = 257;
 	srand(time(NULL));
 	int qid = msgget(ftok(".",'u'), 0);
 	int num=49;
@@ -41,28 +41,26 @@ int main()
 	buf msg;
 	//
 	buf rcv;
+	rcv.greeting[0] = '1';
 	string message;
 	int size = sizeof(msg) - sizeof(long);
-	msg.m_type = 997;
-	do
-	{
-		num = rand();
-		if(num % alpha == 0)
+	msg.m_type = 257;
+	while (rcv.greeting[0] != 'T'){ //WHILE IT IS NOT "TERMINATE"
+		msgrcv(qid, (struct msgbuf *)&rcv, size, 3, IPC_NOWAIT);
+		//num = rand();
+		num = beta;
+		//cout << "i here!!!" << endl;
+		if(num % beta == 0)
 			{
 				message = toString(num);
 				strncpy(msg.greeting, message.c_str(), size);
 				cout << "Number sent:" << num << endl;
 				msgsnd(qid, (struct msgbuf *)&msg, size, 0);	// sends to DataHub
-				msgrcv(qid, (struct msgbuf *)&rcv, size, 4, 0);	// receives from DataHub
 			}
-		//num = 49;
-
-	}while(num > 50);
+	}
 	cout << "Last number: " << num<< endl;
 
-	//msgrcv(qid, (struct msgbuf *)&rcv, size, 4, 0);
-	//cout << rcv.greeting << endl;
-	strncpy(msg.greeting, "TERMINATE", size);
+
 	msgsnd(qid, (struct msgbuf *)&msg, size, 0);
 	return 0;
 }
