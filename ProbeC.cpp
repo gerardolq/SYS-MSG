95bed7cd4bbd7789a2ff888a512f89327907606e
eunicemk <53715369+eunicemk@users.noreply.github.com>
2020-02-19 20:19:40 -0800

    Add files via upload

---
+++ ProbeC.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<time.h>
+#include<cstdlib>
+#include<unistd.h>
+#include<sstream>
+#include<sys/types.h>
+#include<cstring>
+#include <sys/ipc.h> 
+#include <sys/msg.h>
+
+#include "KILL_PATCH.H"
+
+using namespace std;
+
+struct buf
+{
+	long m_type;
+	char greeting[50];
+};
+
+string toString(int n)
+{
+	string s = "";
+	char c;
+	int last;
+	while(n > 0)
+	{
+		last = n%10;
+		c =  '0' +last;
+		s = c + s;
+		n = n/10;
+	}
+
+	return s;
+}
+
+int main(){
+	int rho = 14969;
+	srand(time(NULL));
+	int qid = msgget(ftok(".",'u'), 0);
+	buf msg;
+	string message;
+	int size = sizeof(msg) - sizeof(long);
+	msg.m_type = 90;
+	int num = getpid();
+	message = toString(num);
+	strncpy(msg.greeting, message.c_str(), size);
+	msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+
+	while (true){ //WHILE IT IS NOT "TERMINATE"
+		num = rand();
+		if(num % rho == 0)
+			{
+				cout << "Message sent from: " << getpid() << endl;
+				message = toString(num);
+				strncpy(msg.greeting, message.c_str(), size);
+				cout << "Number sent:" << num << endl;
+				msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+			}
+	}
+
+	cout << "Last number: " << num<< endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
