7984332fd7c4debbf059faf8c2f479c16e558ec9
gerardolq <58379792+gerardolq@users.noreply.github.com>
2020-02-28 13:44:04 -0800

    Update ProbeC.cpp

--- ProbeC.cpp
+++ ProbeC.cpp
@@ -12,12 +12,16 @@
 
 using namespace std;
 
+
+//Our message struct
 struct buf
 {
 	long m_type;
 	char greeting[50];
 };
 
+//used to convert our numbers to a string so that
+//we can send it DataHub
 string toString(int n)
 {
 	string s = "";
@@ -34,32 +38,65 @@ string toString(int n)
 	return s;
 }
 
+//--------------------------------------------------------------------------//
+//------------------------------MAIN function-------------------------------//
+//--------------------------------------------------------------------------//
 int main(){
+	//setting up our rho
 	int rho = 2001463;
+	
+	//seeding out rand with time input
 	srand(time(NULL));
+	
+	//grabbing id of the queue
 	int qid = msgget(ftok(".",'u'), 0);
+	
+	//---------------------setting up message sending variables ---------------------
+	//-message send variable
 	buf msg;
+	//string used to copy number input to our msg
 	string message;
+	//setting up size
 	int size = sizeof(msg) - sizeof(long);
+	//setting up m type
 	msg.m_type = 90;
-	int num = getpid();
+	//number used to hold input
+	int num;
+	//---------------------set up complete ---------------------------------
+	
+	//---------------------sending the pid to Data Hub ---------------------------
+	num = getpid();
 	message = toString(num);
 	strncpy(msg.greeting, message.c_str(), size);
 	msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+	//---------------------PID sent to Data Hub ----------------------------------
+	
+	//------------------Setting up Killpatch with "kill" message -------------------
 	strncpy(msg.greeting, "kill", size);
 	kill_patch(qid, (struct msgbuf *)&msg,size, 1);
-	msg.m_type = 90;
+	//---------------------Done setting up killpatch -------------------------
+	
 
-	while (true){ //WHILE IT IS NOT "TERMINATE"
+	while (true)
+	{ //WHILE IT IS NOT "TERMINATE"
+		//grab new input
 		num = rand();
+		//validate input
 		if(num % rho == 0)
-			{
-				message = toString(num);
-				strncpy(msg.greeting, message.c_str(), size);
-				msgsnd(qid, (struct msgbuf *)&msg, size, 0);
-			}
+		{
+			//----------------------------begin to send message ------------------------
+			//translate our number into a string
+			message = toString(num);
+			//wrap our message up nicely
+			strncpy(msg.greeting, message.c_str(), size);
+			//send to DATA HUB
+			msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+			//-----------------------------message sent-----------------------------
+		}
 	}
-
+	
+	// Adíos ...
+	return 0;
 }
 
 
